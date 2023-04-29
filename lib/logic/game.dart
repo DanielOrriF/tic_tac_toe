5639ec9cf2ce90804e5ee3d26c16db5f87a7fcd3
DanielOrriF <dannif2830@gmail.com>
2023-04-29 13:51:08 +0000

    working game!

--- lib/logic/game.dart
+++ lib/logic/game.dart
@@ -1,10 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Player {
+  static const x = "X";
+  static const o = "O";
+  static const empty = '';
+}
+
 class Game {
   static final boardLength = 9;
+  List<String> board = [];
 
-  //TODO:
-  // - Player
-  // - Game
-  //  - variables - whos turn it is?
-  //  - board
-  //  - winnerCheck()
+  static List<String> initGameBoard() =>
+      List.generate(boardLength, (index) => Player.empty);
+
+  bool winnerCheck(String player, int index, List<List<String>> scoreboard) {
+    bool result = false;
+    int scoreCount = 0;
+    List<int> arrayIndex = [0, 0];
+    if (index < 3) {
+      arrayIndex = [0, index];
+    } else if (index <= 5) {
+      arrayIndex = [1, index - 3];
+    } else {
+      arrayIndex = [2, index - 6];
+    }
+    scoreboard[arrayIndex[0]][arrayIndex[1]] = player;
+    scoreboard[arrayIndex[0]].forEach((element) {
+      if (element == player) {
+        print('scoreCount1 $scoreCount');
+        scoreCount++;
+      }
+    });
+    if (scoreCount != 3) {
+      scoreCount = 0;
+      for (int n = 0; n < scoreboard[arrayIndex[0]].length; n++) {
+        if (scoreboard[n][arrayIndex[1]] == player) {
+          print('scoreCount" $scoreCount');
+          scoreCount++;
+        }
+      }
+    }
+    print(scoreCount);
+    if (scoreCount == 3) {
+      result = true;
+    }
+    return result;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ui/colors.dart';
 import 'logic/game.dart';
+import 'ui/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -31,13 +30,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _GameScreen extends State<MyHomePage> {
-
+  String value = 'X';
+  bool gameOver = false;
+  String result = '';
+  int turn = 0;
+  List<List<String>> scoreboard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
   Game game = Game();
 
-  void initState(){
+  void initState() {
     super.initState();
-    //TODO - inititalize game board
-    print('game');
+    game.board = Game.initGameBoard();
+    print('game $game');
+    print(game.board);
   }
 
   @override
@@ -49,47 +57,83 @@ class _GameScreen extends State<MyHomePage> {
       backgroundColor: MainColor.background,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
               Container(
-                width: 400, //TODO - setja í breytu
-                height: 400, //TODO - setja í breytu
-                child: GridView.count(
-                  crossAxisCount: 9 ~/ 3, //TODO - setja í breytur
-                  padding: EdgeInsets.all(16.0),
-                  mainAxisSpacing: 8.0,
-                  crossAxisSpacing: 8.0,
-                  children: List.generate(9, (index) {
-                    return InkWell(
-                      child: Container(
-                        width: 200, //TODO - setja í breytu
-                        height: 200,//TODO - setja í breytu
-                        decoration: BoxDecoration(
-                          color: MainColor.white,
-                          borderRadius: BorderRadius.circular(16.0),
-                        ),
-                      )
-                    );
-                  }
-
-                )
-                )
-              ),
-            ElevatedButton.icon(
-              onPressed: () {
-                //TODO - setState()
-                /* setState(() {
-
-                });*/
-              },
-              icon: Icon(Icons.replay),
-              label: Text('Repeat game'),
-            )
-          ]
-        ),
-
+                  width: 400,
+                  height: 400,
+                  child: GridView.count(
+                      crossAxisCount: 9 ~/ 3,
+                      padding: EdgeInsets.all(16.0),
+                      mainAxisSpacing: 8.0,
+                      crossAxisSpacing: 8.0,
+                      children: List.generate(9, (index) {
+                        return InkWell(
+                          onTap: () {
+                            setState(() {
+                              if (game.board[index].isEmpty) {
+                                game.board[index] = value;
+                              }
+                              turn++;
+                              if (gameOver) {
+                                result = 'Game is over $value is the loser!';
+                              } else if (!gameOver && turn == 9) {
+                                gameOver = true;
+                                result = 'It is a draw';
+                                gameOver = true;
+                              } else {
+                                gameOver =
+                                    game.winnerCheck(value, index, scoreboard);
+                              }
+                              if (value == 'X') {
+                                value = 'O';
+                              } else {
+                                value = 'X';
+                              }
+                            });
+                          },
+                          child: Container(
+                            width: 200,
+                            height: 200,
+                            decoration: BoxDecoration(
+                              color: MainColor.white,
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                            child: Center(
+                              child: Text(
+                                game.board[index],
+                                style: TextStyle(
+                                    fontSize: 64,
+                                    color: game.board[index] == 'X'
+                                        ? MainColor.x
+                                        : MainColor.o),
+                              ),
+                            ),
+                          ),
+                        );
+                      }))),
+              Text(result),
+              ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    game.board = Game.initGameBoard();
+                    value = 'X';
+                    result = '';
+                    gameOver = false;
+                    scoreboard = [
+                      ["", "", ""],
+                      ["", "", ""],
+                      ["", "", ""],
+                    ];
+                  });
+                },
+                icon: Icon(Icons.replay),
+                label: Text(
+                  'Repeat game',
+                ),
+              )
+            ]),
       ),
     );
   }
 }
-
